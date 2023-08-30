c3c968a6bb6359b189429b63ab06253b97543a9f
Hamza Azman <hamzaazman@outlook.com>
2023-08-30 14:15:22 +0300

    fix: add AndroidEntryPoint annotation to ProfileFragment

--- app/src/main/java/com/example/e_commerce/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/e_commerce/ui/profile/ProfileFragment.kt
@@ -1,31 +1,35 @@
 package com.example.e_commerce.ui.profile
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
-import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import com.example.e_commerce.R
 import com.example.e_commerce.databinding.FragmentProfileBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 
+@AndroidEntryPoint
 class ProfileFragment : Fragment() {
-    private val viewModel : ProfileViewModel by viewModels()
+    private val viewModel: ProfileViewModel by viewModels()
 
-    private lateinit var binding : FragmentProfileBinding
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_profile_,container,false)
+    private lateinit var binding: FragmentProfileBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_profile_, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        viewModel.getUser()
+        viewModel.getUser().observe(viewLifecycleOwner) {
+            println(it.name)
+        }
     }
 }
